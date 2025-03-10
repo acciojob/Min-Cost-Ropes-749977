d8a65f6a917b38c2e86e2f1bdbea12d96143b75f
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-10 14:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    let minHeap = [...arr].sort((a, b) => a - b);
+    let cost = 0;
+
+    while (minHeap.length > 1) {
+        let first = minHeap.shift();
+        let second = minHeap.shift();
+        let sum = first + second;
+        cost += sum;
+        minHeap.push(sum);
+        minHeap.sort((a, b) => a - b);
+    }
+    
+    return cost;
 }
 
-module.exports=mincost;
+function calculateMinCost() {
+    let input = document.getElementById("ropes").value;
+    let ropes = input.split(",").map(num => parseInt(num.trim())).filter(num => !isNaN(num));
+    if (ropes.length === 0) {
+        document.getElementById("result").innerText = "Please enter valid numbers.";
+        return;
+    }
+    let result = mincost(ropes);
+    document.getElementById("result").innerText = "Minimum cost: " + result;
+}
